7b6331e7dd362f056339405aa24cb7cf3afcb46a
tamadekk <186jnd@gmail.com>
2024-11-28 00:53:06 +0100

    feat: data population

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Therapist
+from .models import UserProfile, MoodEntry, Therapist
 
+admin.site.register(UserProfile)
+admin.site.register(MoodEntry)
 admin.site.register(Therapist)
